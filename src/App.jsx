08d1614aff51c413d70a22629475a09f6f40d500
JaimeMadFer <61852686+JaimeMadFer@users.noreply.github.com>
2022-05-03 18:42:56 +0200

    busqueda mas bonita y con los parámetros que se introduzcan

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,57 @@
 import React, { useState, useRef } from 'react';
-import './estilos.css';
+import './components/estilos.css';
 import { ApiUtil } from './components/ApiUtil';
 
 export function App()
 {
+    const [datosConsulta, setdatosConsulta] = useState(
+        { authors: "", title: "", type: "all" }
+    );
 
-    const searchType = useRef();
+    const bookAuthors = useRef();
+    const bookTitle = useRef();
+    const [typeSearch, setType] = useState("all");
 
-    // const [todos, setTodos] = useState([
-    //     {id: 1}
-    // ]);
+    const search = (state) => {
+        const authors = bookAuthors.current.value;
+        const title = bookTitle.current.value;
 
-    const search = () => {
-        const type = searchType.current.value;
-        <p>{type}</p>
-        return type;
+        setdatosConsulta(() => {
+            return { authors, title, typeSearch };
+        });
+
     };
+
+    const handleChange = (event) => {
+        setType( event.target.value, event.target.checked );
+    };
+
 
     return (
         <React.Fragment>
             
-            <p><input type="text" placeholder="Book authors" /></p>
-            <p><input type="text" placeholder="Book title" /></p>
+            <p><input ref={bookAuthors} type="text" placeholder="Book authors" /></p>
+            <p><input ref={bookTitle} type="text" placeholder="Book title" /></p>
     
-            <p><button type="button" onClick={search}>Search books</button></p>
+            <p><button onClick={search}>Search books</button></p>
     
             <div className="allTabs">
                 <div className="tab">
-                    <input ref={searchType} type="radio" id="all" name="type_search" value="all" defaultChecked></input>
+                    <input onChange={handleChange} type="radio" name="type" value="all" defaultChecked></input>
                     <label htmlFor="all">all</label>
                 </div>
                 <div className="tab">
-                    <input ref={searchType} type="radio" id="books" name="type_search" value="books"></input>
+                    <input onChange={handleChange} type="radio" name="type" value="books"></input>
                     <label htmlFor="books">books</label>
                 </div>
                 <div className="tab">
-                    <input ref={searchType} type="radio" id="magazines" name="type_search" value="magazines"></input>
+                    <input onChange={handleChange} type="radio" name="type" value="magazines"></input>
                     <label htmlFor="magazines">magazines</label>
                 </div>
             </div>
             <br />
             
-            <ApiUtil />
+            <ApiUtil datosConsulta={datosConsulta} onClick={search}/>
         
         </React.Fragment>
     );
--- src/components/ApiUtil.jsx
+++ src/components/ApiUtil.jsx
@@ -1,42 +1,88 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 
-export function ApiUtil()
+export function ApiUtil({datosConsulta})
 {
-    const baseUrl = "https://www.googleapis.com/books/v1/volumes";
+    // if (datosConsulta == null) return;
+
+    const baseUrl = "https://www.googleapis.com/books/v1/volumes?";
 
     const fetchApi = async (state) => 
     {
-        await axios.get(baseUrl, {params: { q: "padrino", printType: "all", maxResults: 5 } })
+        // console.log(datosConsulta);
+        
+        // let datos = datosConsulta.authors + datosConsulta.title;
+        // if (datosConsulta.authors == "")
+        // {
+        //     datos = datosConsulta.title;
+        // }
+        // else if (datosConsulta.title = "")
+        // {   
+        //     datos = " +inauthor:datosConsulta.authors"
+        // }
+
+        // q: {inauthor: datosConsulta.authors, intitle: datosConsulta.title}
+        //await axios.get(baseUrl, {params: { q: datos, printType: datosConsulta.typeSearch, maxResults: 5 } })
+        await axios.get(`https://www.googleapis.com/books/v1/volumes?q=+intitle:${datosConsulta.title}+inauthor:${datosConsulta.authors}&printType=${datosConsulta.typeSearch}&maxResults=20`)
         .then((res) => {
             state(res.data.items);
-            console.log(res.data.items);
+            // console.log(res.data.items)
         });
     }
 
     const [libros, setLibros] = useState(null);
 
-    useEffect(() => {
+    useEffect(() => 
+    {
         fetchApi(setLibros);
-    }, [])
+    },
+    [datosConsulta])
 
 
-  return (
-    <>
-        {libros != null ? (
-            libros.map(libro => (
-                <div key={libro.id}>
-                    {/* {libro.volumeInfo.imageLinks.smallThumbnail != null ? 
-                        (<img src={libro.volumeInfo.imageLinks.smallThumbnail} alt=""></img>)
-                    : ('No hay imagen')} */}
-
-                    <h4>{libro.volumeInfo.title}</h4>
-                </div>
+    const listarAutores = (authors) =>
+    {
+        let final = [];
+        if (authors != null)
+        {
+            console.log(authors);
+            authors.map((autor, index) => (
+                <>
+                    {final.push(autor)}
+                    {final.push(", ")}
+                </>
             ))
-            ) : 
-            ('No results found')
+            final.pop();
         }
+        return final;
+    }
 
-    </>
-  )
+    return (
+        <>
+            {libros != null ? (
+                libros.map((libro) => (
+                    <div key={libro.id}>
+
+                        <div className='contenedorCaja'>
+                            <div className="caja-principal">
+                                <div className="imagen">
+                                    {libro.volumeInfo.imageLinks != null ? (
+                                        <img src={libro.volumeInfo.imageLinks.smallThumbnail} width="80" height="120" />
+                                    ) :
+                                    (<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.zBarRG-ls2zAg_pO52oRHwAAAA%26pid%3DApi&f=1" width="87" height="94"/>)}
+                                </div>
+                                <div className="texto-caja">
+                                    <p><a href={libro.volumeInfo.infoLink}>{libro.volumeInfo.title}</a></p>
+                                    {listarAutores(libro.volumeInfo.authors)}
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                ))
+                ) : 
+                ('No results found')
+            }
+
+        </>
+    )
 }
